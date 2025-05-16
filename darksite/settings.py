94c20f08510b97021378da22c0909930cda30c2e
Merzuev <merzuev2@mail.ru>
2025-05-16 15:27:25 +0200

    Configure static files for deployment

--- darksite/settings.py
+++ darksite/settings.py
@@ -115,12 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # это для collectstatic
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main', 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'main/static']
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'main/templates']
